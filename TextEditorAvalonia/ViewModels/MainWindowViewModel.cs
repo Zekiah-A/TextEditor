742af31c89818e07a739604636cd1fe0c3d03c9f
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2021-05-17 10:31:41 +0300

    Add template UI plan

--- TextEditorAvalonia/ViewModels/MainWindowViewModel.cs
+++ TextEditorAvalonia/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace TextEditorAvalonia.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
     }
 }

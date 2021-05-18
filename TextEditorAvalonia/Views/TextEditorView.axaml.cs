7fd1c183d0226fcd17c0387902019b2d42803124
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2021-05-18 04:22:56 +0300

    Add TextEditor view and VM

---
+++ TextEditorAvalonia/Views/TextEditorView.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TextEditorAvalonia.Views
+{
+    public partial class TextEditorView : UserControl
+    {
+        public TextEditorView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
